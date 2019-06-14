bb85bb95b25c22140c5769bf9372d73f7bc57b9e
ivan <lx1312315409@gmail.com>
2019-06-14 14:50:34 +0800

    测试

---
+++ pages/about/_id.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>About Id content</h2>
+    <p>Id:{{ $route.params.id }}</p>
+    <ul>
+      <li>
+        <a href="/about">about</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return /\d+$/.test(params.id);
+  },
+  data() {
+    return {
+      title: this.$route.params.title
+    };
+  },
+  created() {
+      console.log(this.title)
+  },
+  head() {
+    return {
+        title:this.title,
+        meta:[
+            {hid:'description',name:'About',content:'This is about Ivan'}
+        ]
+    };
+  }
+};
+</script>
---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>About Index Page</h2>
+    <ul>
+        <li><nuxt-link to="/">home</nuxt-link></li>
+        <li><nuxt-link :to="{name:'about-id',params:{id:131231,title:'AboutIvan'}}">131231</nuxt-link></li>
+        <li><nuxt-link :to="'/about/'+ 'cavielle'">cavielle</nuxt-link></li>
+        <li>
+        <nuxt-link :to="{name:'index-article-id',params:{id:131231,title:'【ivan Blog】'}}">【这是标题】这是标题这是标题，这是标题这是标题这是标题这是标题。这是标题这是标题。</nuxt-link>
+
+        </li>
+        
+    </ul>
+    </div>
+</template>
+
